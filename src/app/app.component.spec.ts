3d4ac00159056d9077e7a77f50d8e71b03256f47
Madalina Rosca <madalina.rosca.bp@emeal.nttdata.com>
2024-02-13 19:54:46 +0200

    deployed on firebase

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'expense-tracker-standalone' title`, () => {
+  it(`should have the 'expense-tracker-a17' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('expense-tracker-standalone');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, expense-tracker-standalone');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, expense-tracker-a17'
+    );
   });
 });
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
-import { UpperCasePipe } from '@angular/common';
-import { Router, RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NgClass, UpperCasePipe } from '@angular/common';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RoutesRecognized,
+} from '@angular/router';
 import { DaysService } from '../../shared/days.service';
 
 @Component({
@@ -8,15 +13,29 @@ import { DaysService } from '../../shared/days.service';
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
-  imports: [UpperCasePipe, RouterLink],
+  imports: [UpperCasePipe, RouterLink, NgClass],
 })
-export class SidebarComponent {
-  constructor(private daysService: DaysService, private router: Router) {}
+export class SidebarComponent implements OnInit {
+  activeItem: string = '/home';
+  constructor(
+    private daysService: DaysService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activeItem = this.router.url;
+    this.router.events.subscribe((data) => {
+      if (data instanceof RoutesRecognized) {
+        this.activeItem = data.state.root.firstChild.url[0].path;
+      }
+    });
+  }
 
   onMenuItemClick() {
-    console.log(this.router.url);
     const index =
       this.router.url === '/home' ? 0 : this.router.url === '/summary' ? 8 : 1;
+    this.activeItem = this.router.url;
     this.daysService.setSelectedDay(index);
   }
 }
